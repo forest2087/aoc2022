dac8be84c58901b01bf15b8f66e310aa3973f12a
4st <forest2087@gmail.com>
2022-12-12 17:08:19 +0000

    removed some useless code

--- 12/day12.py
+++ 12/day12.py
@@ -7,27 +7,10 @@ def readInput(f):
     
         if not ln:
             break
-        # print(list(map(lambda x: -1 if x==83 else -2 if x== 69 else ord(x)-97, [x for x in ln.split()[0]])))
-        # print([ord(x)-97 for x in ln.split()[0]])
         grid.append([ord(x)-97 for x in ln.split()[0]])
+        
     return grid
 
-class Node:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def x(self):
-        return self.x
-
-    def y(self):
-        return self.y
-
-    def get(self):
-        return {self.x, self.y}
-
-    def __repr__(self):
-        return 'x:' + str(self.x) + ', y:' + str(self.y)
 
 START = -14
 END = -28
@@ -37,7 +20,6 @@ def checkCord(dx, dy, s, grid, curNum):
 
     if dx<0 or dx>=R or dy<0 or dy>=C:
         return False
-
     
     destination = grid[dx][dy]
     if destination==START:
@@ -60,19 +42,13 @@ def solv1(f):
     q = queue.Queue()
     s = set()
 
-    # START_CORD = ()
-    # END_CORD = ()
     #get starting pos
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]==START:
                 q.put((i,j))
                 s.add((i, j))
-                break
-                # START_CORD = (i,j)
-            # if grid[i][j]==END:
-                # END_CORD = (i, j)
-                
+                break                
 
     step = 0
     offset = [1, 0, -1, 0, 1]
